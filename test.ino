23b0e86138333b441d3bfc396690ef49c0504c36
Inotfest <70272315+Inotfest@users.noreply.github.com>
2020-12-01 22:00:43 +0200

    Add files via upload

---
+++ test.ino
@@ -0,0 +1,49 @@
+#include <LiquidCrystal.h>
+#include <EEPROM.h>
+
+
+
+
+
+unsigned long lastTime = 0;
+float speed = 0;
+float length = 2.04;
+float totalKm = 16839;
+
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 8;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP);
+  attachInterrupt(0, sens, FALLING);
+  lcd.begin(16, 2);
+  lcd.setCursor(3, 0);
+  lcd.print("Hello, Dima");
+  delay(5000);
+  lcd.clear();
+  
+}
+
+void sens(){
+  if(millis() - lastTime > 99 ){
+    speed = length / ((float)(millis() - lastTime) / 1000) * 3.6;
+    lastTime = millis();
+    totalKm = totalKm + length;
+  }
+  
+}
+
+
+void loop() {
+  delay(500);
+  lcd.setCursor(5, 0);
+  lcd.print(speed, 0);
+  lcd.setCursor(8, 0);
+  lcd.print("km/h");
+  lcd.setCursor(0, 1);
+  lcd.print("t:");
+  lcd.setCursor(2, 1);
+  lcd.print(totalKm / 1000, 3);
+  lcd.print("    ");
+}
