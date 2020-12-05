56a6bc253e89fe79b4d75624f4b73adeb14be65b
Inotfest <70272315+Inotfest@users.noreply.github.com>
2020-12-05 15:59:01 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -1,14 +1,10 @@
 #include <LiquidCrystal.h>
 #include <EEPROM.h>
 
-
-
-
-
 unsigned long lastTime = 0;
 float speed = 0;
-float length = 2.04;
-float totalKm = 16839;
+float wheelLength = 2.04;
+float totalKm = EEPROM.get(0, totalKm);
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 8;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
@@ -20,23 +16,26 @@ void setup() {
   lcd.begin(16, 2);
   lcd.setCursor(3, 0);
   lcd.print("Hello, Dima");
-  delay(5000);
+  delay(3000);
   lcd.clear();
-  
+  lcd.setCursor(3,0);
+  lcd.print("What's up?");
+  delay(3000);
+  lcd.clear();
 }
 
 void sens(){
   if(millis() - lastTime > 99 ){
-    speed = length / ((float)(millis() - lastTime) / 1000) * 3.6;
+    speed = wheelLength / ((float)(millis() - lastTime) / 1000) * 3.6;
     lastTime = millis();
-    totalKm = totalKm + length;
+    totalKm = totalKm + wheelLength; 
   }
   
 }
 
-
 void loop() {
   delay(500);
+  EEPROM.put(0, totalKm);
   lcd.setCursor(5, 0);
   lcd.print(speed, 0);
   lcd.setCursor(8, 0);
