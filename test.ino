a9c1551298222d7acaab0de65b9b75622cc8be81
Inotfest <70272315+Inotfest@users.noreply.github.com>
2020-12-05 17:51:31 +0200

    Update test.ino

--- test.ino
+++ test.ino
@@ -11,8 +11,10 @@ LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
   Serial.begin(9600);
+  
   pinMode(2, INPUT_PULLUP);
   attachInterrupt(0, sens, FALLING);
+  
   lcd.begin(16, 2);
   lcd.setCursor(3, 0);
   lcd.print("Hello, Dima");
@@ -35,7 +37,9 @@ void sens(){
 
 void loop() {
   delay(500);
+  
   EEPROM.put(0, totalKm);
+  
   lcd.setCursor(5, 0);
   lcd.print(speed, 0);
   lcd.setCursor(8, 0);
@@ -45,4 +49,10 @@ void loop() {
   lcd.setCursor(2, 1);
   lcd.print(totalKm / 1000, 3);
   lcd.print("    ");
+  
+  if((millis() - lastTime) > 2000){
+    speed = 0;
+    lcd.setCursor(6, 0);
+    lcd.print(" ");
+  }
 }
